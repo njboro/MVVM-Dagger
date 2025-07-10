98a0712ebd95efb4ddd6da0b322fcf58b68283cb
Nabajyoti Boro <nabajyotiboro@gmail.com>
2025-07-10 19:22:23 +0530

    Wrong import in CountryDB.kt corrected.

--- app-mvvm-compose/src/main/java/com/example/app_mvvm_compose/db/CountryDB.kt
+++ app-mvvm-compose/src/main/java/com/example/app_mvvm_compose/db/CountryDB.kt
@@ -2,9 +2,9 @@ package com.example.app_mvvm_compose.db
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
-import com.example.mvvm_dagger.model.Country
+import com.example.app_mvvm_compose.model.Desh
 
-@Database(entities = [Country::class], version = 1)
+@Database(entities = [Desh::class], version = 1)
 abstract class CountryDB : RoomDatabase(){
     abstract fun getCountryDAO() : CountryDAO
 }
